15fb2e4b430e7d0327258975c498e936a60957a0
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-03 11:03:33 +0200

    Show toast with the edit text value

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.dgarciasarai.trainingappkotlin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    button.setOnClickListener {
+      val text = editText.text
+      Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
   }
 }
