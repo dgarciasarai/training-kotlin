f5be9e784abe53d0465a3074650de8520fda274c
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-05 11:52:50 +0200

    Default value in constructor

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/Extensions.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/Extensions.kt
@@ -6,6 +6,6 @@ import android.widget.Toast
 /**
  * @author Sarai Díaz García
  */
-fun Context.toast(text: CharSequence) {
-    Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+fun Context.toast(text: CharSequence, length: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, text, length).show();
 }
