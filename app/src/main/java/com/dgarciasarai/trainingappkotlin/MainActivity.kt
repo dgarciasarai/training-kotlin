df261598b9dc6c6372e600ff36a09979e88078e2
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-05 11:50:53 +0200

    Use toast extension function

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/MainActivity.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/MainActivity.kt
@@ -2,8 +2,6 @@ package com.dgarciasarai.trainingappkotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             val text = editText.text
-            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-        }
-
-        button.setOnClickListener {
-            val text = editText.text
-            Toast.makeText(this@MainActivity, text, Toast.LENGTH_SHORT).show()
+            toast(text)
         }
     }
 }
