66742852f1de8edded33f6df1f19030b8e9e3f9e
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-05 11:56:19 +0200

    Move params to constructor

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/Item.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/Item.kt
@@ -3,15 +3,4 @@ package com.dgarciasarai.trainingappkotlin
 /**
  * @author Sarai Díaz García
  */
-class Item {
-    
-    var title: String = ""
-        get() {
-            return "title: $field"
-        }
-        set(value) {
-            field = "title: $value"
-        }
-
-    val url: String = ""
-}
+class Item(var title: String, val url: String)
