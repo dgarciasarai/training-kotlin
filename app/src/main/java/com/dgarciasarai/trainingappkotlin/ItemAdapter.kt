4456912e1ba1ce00057f4f89bac9df9ced527870
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-07 20:33:33 +0200

    Create ItemAdapter for the recycler view

---
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/ItemAdapter.kt
@@ -0,0 +1,27 @@
+package com.dgarciasarai.trainingappkotlin
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * @author Sarai Díaz García
+ */
+class ItemAdapter(val items: List<Item>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+}
