691abadf8702bd2647ccb4195a5ac25202c9fd18
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-07 20:54:20 +0200

    Fill onBindViewHolder method in adapter

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/ItemAdapter.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/ItemAdapter.kt
@@ -8,15 +8,18 @@ import kotlinx.android.synthetic.main.view_item.view.*
 /**
  * @author Sarai Díaz García
  */
-class ItemAdapter(val items: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(val items: List<Item>,
+    val listener: (Item) -> Unit) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(parent.inflate(R.layout.view_item))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) = with(holder.itemView) {
-            item_title.text = items[position].title
-            item_image.loadUrl(items[position].url)
+        val item = items[position]
+        item_title.text = item.title
+        item_image.loadUrl(item.url)
+        setOnClickListener { listener(item) }
     }
 
     override fun getItemCount() = items.size
