c697ffe423850c7a0cbc529f42407b0fa16b7a9b
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-07 20:41:32 +0200

    Create extension to load images with picasso

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/Extensions.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/Extensions.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.Toast
+import com.squareup.picasso.Picasso
 
 /**
  * @author Sarai Díaz García
@@ -15,4 +17,8 @@ fun Context.toast(text: CharSequence, length: Int = Toast.LENGTH_SHORT) {
 
 fun ViewGroup.inflate(layoutRes: Int): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, false)
+}
+
+fun ImageView.loadUrl(url: String) {
+    Picasso.with(context).load(url).into(this)
 }
