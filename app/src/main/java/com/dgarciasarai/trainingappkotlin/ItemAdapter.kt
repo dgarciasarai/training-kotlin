a6063dc7fd83721d96cd0a608e18539fe9567408
Sarai Díaz <dgarciasarai@gmail.com>
2017-04-07 20:41:52 +0200

    Fill title and image with the items data

--- app/src/main/java/com/dgarciasarai/trainingappkotlin/ItemAdapter.kt
+++ app/src/main/java/com/dgarciasarai/trainingappkotlin/ItemAdapter.kt
@@ -1,21 +1,22 @@
 package com.dgarciasarai.trainingappkotlin
 
 import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.view_item.view.*
 
 /**
  * @author Sarai Díaz García
  */
-class ItemAdapter(val items: List<Item>): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+class ItemAdapter(val items: List<Item>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(parent.inflate(R.layout.view_item))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-
+        holder.itemView.item_title.text = items[position].title
+        holder.itemView.item_image.loadUrl(items[position].url)
     }
 
     override fun getItemCount() = items.size
